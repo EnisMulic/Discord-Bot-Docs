51df8b447a447d36fcf29ecc2283298a9d2e4d89
Enis Mulić <enis.s.mulic@gmail.com>
2021-01-15 23:28:41 +0100

    Add Channel

---
+++ src/components/Channel/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import style from "./Channel.module.css";
+
+const Channel = props => {
+    return (
+        <div className={style.Channel}>
+           # {props.name}
+        </div>
+    )
+}
+
+Channel.propTypes = {
+    name: PropTypes.string.isRequired
+}
+
+export default Channel
