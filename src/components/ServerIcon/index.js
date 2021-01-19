d2087f5f1d431a3044345eafc5dc4103a15393c5
Enis Mulić <enis.s.mulic@gmail.com>
2021-01-19 22:18:55 +0100

    Add icons to server navbar

--- src/components/ServerIcon/index.js
+++ src/components/ServerIcon/index.js
@@ -8,14 +8,14 @@ import Logo from '../../assets/discord-logo.png';
 const ServerIcon = (props) => {
     return (
         <div className={style.ServerIcon}>
-            <img src={Logo} alt={props.name} />
+            {props.children || <img src={Logo} alt={props.name} />}
         </div>
     );
 };
 
 ServerIcon.propTypes = {
     name: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
+    icon: PropTypes.string,
 };
 
 export default ServerIcon;
--- src/components/ServerNavbar/index.js
+++ src/components/ServerNavbar/index.js
@@ -3,12 +3,28 @@ import ServerIcon from '../ServerIcon';
 
 import style from './ServerNavbar.module.css';
 
+import { ReactComponent as AddServer } from '../../assets/circle-plus.svg';
+import { ReactComponent as DiscoverServer } from '../../assets/discover.svg';
+import { ReactComponent as Discover } from '../../assets/discord.svg';
+
 export const ServerNavbar = () => {
     return (
         <div className={style.ServerNavbar}>
+            <ServerIcon name="Test">
+                <Discover className={style.Home} />
+            </ServerIcon>
+            <div className={style.Break}>
+                <div></div>
+            </div>
             <ServerIcon name="Test" icon="discord-logo.png" />
             <ServerIcon name="Test" icon="discord-logo.png" />
             <ServerIcon name="Test" icon="discord-logo.png" />
+            <ServerIcon name="Add a Server">
+                <AddServer className={style.Server} />
+            </ServerIcon>
+            <ServerIcon name="Discover Public Servers">
+                <DiscoverServer className={style.Server} />
+            </ServerIcon>
         </div>
     );
 };
