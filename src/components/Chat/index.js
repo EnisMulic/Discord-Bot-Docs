0a48d7fa60539366f014efcc535541138d698e07
Enis Mulić <enis.s.mulic@gmail.com>
2021-01-19 21:27:05 +0100

    Update chat style

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -7,22 +7,25 @@ import style from './Chat.module.css';
 const Chat = () => {
     return (
         <div className={style.Chat}>
+            <div className={style.Spacer} />
+            <div className={style.Messages}>
+                <Message
+                    content="Test Test Test Test"
+                    name="User 1"
+                    color="#6a0dad"
+                />
+                <Message
+                    content="Test Test Test Test"
+                    name="User 2"
+                    color="#6a0dad"
+                />
+                <Message
+                    content="Test Test Test Test"
+                    name="User 3"
+                    color="#6a0dad"
+                />
+            </div>
             <MessageBar />
-            <Message
-                content="Test Test Test Test"
-                name="User 1"
-                color="#6a0dad"
-            />
-            <Message
-                content="Test Test Test Test"
-                name="User 2"
-                color="#6a0dad"
-            />
-            <Message
-                content="Test Test Test Test"
-                name="User 3"
-                color="#6a0dad"
-            />
         </div>
     );
 };
--- src/components/MessageBar/index.js
+++ src/components/MessageBar/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import { ReactComponent as PlusIcon } from '../../assets/plus.svg';
 import style from './MessageBar.module.css';
 
 const MessageBar = () => {
-    return <div className={style.MessageBar}></div>;
+    return (
+        <div className={style.MessageBar}>
+            <div className={style.PlusIcon}>
+                <PlusIcon />
+            </div>
+            <div className={style.TextBar}></div>
+        </div>
+    );
 };
 
 export default MessageBar;
